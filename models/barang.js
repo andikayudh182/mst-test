38bacab20a499c98a7d3e244e37fcf1d1744f92f
andikayudh182 <andika.rianto2000@gmail.com>
2023-05-13 23:39:27 +0700

    cek select barang

--- models/barang.js
+++ models/barang.js
@@ -1,27 +1,44 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Barang extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Barang.init({
-    kode: DataTypes.STRING,
-    nama: DataTypes.STRING,
-    harga: DataTypes.DECIMAL
-  }, {
-    sequelize,
-    timestamps:false,
-    freezeTableName:true,
-    modelName: 'Barang',
-  });
-  return Barang;
-};
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Barang extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   Barang.init({
+//     kode: DataTypes.STRING,
+//     nama: DataTypes.STRING,
+//     harga: DataTypes.DECIMAL
+//   }, {
+//     sequelize,
+//     timestamps:false,
+//     freezeTableName:true,
+//     modelName: 'Barang',
+//   });
+//   return Barang;
+// };
+
+const Sequelize = require('sequelize');
+const db = require('../config/db.config.js');
+
+const Barang = db.define('m_barang', {
+  kode: {
+    type: Sequelize.STRING
+  },
+  nama: {
+    type: Sequelize.STRING
+  },
+  harga: {
+    type: Sequelize.DECIMAL(10,2)
+  },
+})
+
+module.exports = Barang;
--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,12 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db.config.js');
-const List = require('../models/List');
+// const List = require('../models/List');
+const Barang = require('../models/barang');
 
 router.get('/', async (req, res) => {
   try {
-    const list = await List.findAll()
+    const barang = await Barang.findAll()
     res.status(200).send({
       message:'success',
       data:list
