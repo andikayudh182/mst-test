88fba5c9101e4005bb51249f966a726ed37bbccc
andikayudh182 <andika.rianto2000@gmail.com>
2023-05-13 23:26:21 +0700

    ganti jadi List

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-const barangController = require('../controllers').barang
+// const barangController = require('../controllers').barang
 
-/* Endpoint Barang Controller*/
-router.get('/api/barang', barangController.getAllBarang)
+// /* Endpoint Barang Controller*/
+// router.get('/api/barang', barangController.getAllBarang)
+
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db.config.js');
+const List = require('../models/List');
+
+router.get('/', async (req, res) => {
+  try {
+    const list = await List.findAll()
+    res.status(200).send({
+      message:'success',
+      data:list
+    })
+  } catch(e){
+    res.send
+  }
+})
+
+module.exports = router;
