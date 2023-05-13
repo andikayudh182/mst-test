ab63529e529e8942e3153ed388bc2ca7b818d2c7
andikayudh182 <andika.rianto2000@gmail.com>
2023-05-13 18:57:36 +0700

    test

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 const db = require('./config/db.config.js');
 
-var cors = require('cors-anywhere');
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers: Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization');
-
-  next();
-}
+const corsAnywhere = require('cors-anywhere');
 
 dotenv.config();
 
@@ -25,15 +18,9 @@ app.get('/', (req, res) => res.send('running...'));
 
 app.use('/list', require('./routes/list'));
 
-const corsProxy = cors.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
-});
-
+// Gunakan cors-anywhere sebagai middleware
+const corsProxy = corsAnywhere.create();
 app.use(corsProxy);
-app.options("*", cors()); // Enable preflight request for all routes
-app.use(allowCrossDomain);
 
 const PORT = process.env.PORT || 5000;
 
