5d6e466fc950123c2f86916f88c95d375d10d7cc
andikayudh182 <andika.rianto2000@gmail.com>
2023-05-13 18:52:58 +0700

    tes cors anywhere

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ const path = require('path');
 const dotenv = require('dotenv');
 const db = require('./config/db.config.js');
 
-var cors = require('cors')
+var cors = require('cors-anywhere');
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers: Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization');
-  
+
   next();
 }
-
-
-
 
 dotenv.config();
 
@@ -27,8 +24,15 @@ const app = express();
 app.get('/', (req, res) => res.send('running...'));
 
 app.use('/list', require('./routes/list'));
-app.use(cors());
-app.options("*", cors());
+
+const corsProxy = cors.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+});
+
+app.use(corsProxy);
+app.options("*", cors()); // Enable preflight request for all routes
 app.use(allowCrossDomain);
 
 const PORT = process.env.PORT || 5000;
